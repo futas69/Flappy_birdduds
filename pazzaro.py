a9a9e2b944442ec9aa39be799481745c779eedcc
Ana Julia <131875314+AnaJu09@users.noreply.github.com>
2023-05-10 08:59:12 -0300

    Update1 pazzaro.py

--- pazzaro.py
+++ pazzaro.py
@@ -1,20 +1,25 @@
+#tela
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 800
 
-# o codigo ta deveras poggers
-
-import pygame
-from pygame.locals import *
-
-# tela do jogo
-titulo = 'pazaro'
-screen_width = 640
-screen_height = 480
 
 pygame.init()
-screen = pygame.display.set_mod((screen_width, screen_height))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ 
+BACKGROUND = pygame.image.load('fundo.png')
+BACKGROUND = pygame.transform.scale(BACKGROUND,(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ 
 
 while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
 
-    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+    screen.blit(BACKGROUND, (0,0))
+
+ 
+
+    pygame.display.update()
